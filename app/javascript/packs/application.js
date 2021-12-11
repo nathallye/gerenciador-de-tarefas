e5765f03fa1f05f3e54faf288e02fc51efa12611
Nathallye <nathallyet@gmail.com>
2021-12-11 15:21:39 -0300

    Modificaçao das views e alteraçao do arquivo application.css para application.scss.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "../src/application.css";
+import "../src/application.scss";
 
 Rails.start();
 ActiveStorage.start();
